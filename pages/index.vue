13f16e3ad80e45b131a77d092884c651fdb1e7d9
jairaume <jerome.rascle@student.junia.com>
2023-08-25 23:24:28 +0200

    feat: 🎸 Added responsive text sizes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <main class="bg-grey-900">
     <section aria-labelledby="" class="w-screen h-screen relative">
-      <div class="w-full absolute top-1/2 -translate-y-1/2 text-center">
-        <h1 class="text-orange-100 text-3xl font-monument tracking-widest">&#60;Jérôme/&#62;</h1>
-        <h1 class="text-xl font-extrabold">Site en construction</h1>
+      <div class="responsive-padding-x responsive-padding-y">
+        <div class="bg-grey-50">
+          <h1>Hi there</h1>
+        </div>
       </div>
     </section>
   </main>
