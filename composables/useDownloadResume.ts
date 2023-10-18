eac7d593af4db84d1b01241033fca32e99d29ebe
jairaume <jerome.rascle@student.junia.com>
2023-10-18 21:53:42 +0200

    feat: 🎸 Added mobile devices resume download functionnality

--- composables/useDownloadResume.ts
+++ composables/useDownloadResume.ts
@@ -1,14 +1,23 @@
 export const useDownloadResume = () => {
     const supabase = useSupabaseClient()
-    async function downloadResume(){
-        const { data, error } = await supabase
-            .storage
-            .from('public')
-            .download('images/resume/CV_2023.pdf')
-        if(error) console.error(error)
-        const blob = new Blob([data as BlobPart], {type: 'application/pdf'})
-        const url = window.URL.createObjectURL(blob)
-        return window.open(url)
+    const {public:{supabaseUrl}} = useRuntimeConfig()
+    const { isMobile } = useDevice();
+
+    async function downloadResume() {
+        if (!isMobile) {
+            const {data, error} = await supabase
+                .storage
+                .from('public')
+                .download('images/resume/CV_2023.pdf')
+            if (error) console.error(error)
+            const blob = new Blob([data as BlobPart], {type: 'application/pdf'})
+            const url = window.URL.createObjectURL(blob)
+            return window.open(url)
+        }
+        else {
+            const url = supabaseUrl + '/storage/v1/object/public/images/resume/CV_2023.pdf'
+            return window.open(url, '_blank')
+        }
     }
 
     return { downloadResume }
